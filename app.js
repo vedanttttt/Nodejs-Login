d2aff3e940e2d5d8e66c0e0d4d7deba2a8396626
vedanttttt <51362126+vedanttttt@users.noreply.github.com>
2020-12-03 20:19:00 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+// var path = require('path');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
+
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+//Connect to Mongo
+mongoose.connect(db,{useNewUrlParser: true,useUnifiedTopology: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+//EJS
+app.use(expressLayouts);
+//app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+//Bodyparser
+app.use(express.urlencoded({extended: false})); //to get data from form
+
+// Express session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+//Global Vars
+app.use((req,res,next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+//Routes
+app.use('/',indexRouter);
+app.use('/users',userRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI : 'mongodb+srv://vedant:vedant@cluster0.kt7c3.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
